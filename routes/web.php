aa0fe4984b6c27ee0b12d6d4df860d45ca74304c
Promit Bhattacharjee <145506425+promit-bhattacharjee@users.noreply.github.com>
2023-12-25 20:52:31 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,9 @@
 
 use App\Http\Controllers\homeController;
 use Illuminate\Support\Facades\Route;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 use App\Http\Controllers\HomeController;
 
-Route::get('/', [HomeController::class, 'homePage']);
-
 Route::get('/',[homeController::class,'homePage']);
-Route::get('',[homeController::class,'homePage']);
 Route::get('/home',[homeController::class,'homePage']);
 Route::get('/about',[homeController::class,'aboutPage']);
 Route::get('/projects',[homeController::class,'projectsPage']);
